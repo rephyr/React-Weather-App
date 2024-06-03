0a7af20828b036ff8efcca7550c24cdf684ce4c5
Elmeri Sipola <elmerisipola@hotmail.com>
2024-06-03 15:05:54 +0300

    Api working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState('London');
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+    console.log(apiKey)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+      .then(response => response.json())
+      .then(data => setWeatherData(data));
+  }, [city]);
+
   return (
     <div className="App">
+      <div className="top-bar">
+        <h1>Weather App</h1>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          value={city}
+          onChange={e => setCity(e.target.value)}
+          placeholder="Enter city"
+        />
+        {weatherData && (
+          <div>
+            <h1>{weatherData.name}</h1>
+            <h2>{weatherData.main.temp}</h2>
+          </div>
+        )}
       </header>
     </div>
   );
