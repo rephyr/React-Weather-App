dc45f7a8901c6aaf8ba9c68931084453b874bc8f
Elmeri Sipola <elmerisipola@hotmail.com>
2024-06-06 15:12:02 +0300

    Searchfield changes

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import TextField from '@mui/material/TextField';
 import ExtraWeatherData from './ExtraWeatherData';
+import SearchIcon from '@mui/icons-material/Search';
 import './SideBar.css';
 
 function SideBar({ setCity, weatherData, extraWeatherData }) {
@@ -17,14 +17,24 @@ function SideBar({ setCity, weatherData, extraWeatherData }) {
 
   return (
     <div className="sidebar">
-        <form onSubmit={handleButtonClick}>
-        <div className="search-field">
-          <TextField
-            label="Enter city"
+      <form onSubmit={handleButtonClick}>
+      <div className="search-field">
+      <input
+            type="text"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
-          />
-        </div>
+            style={{
+                backgroundColor: 'white',
+                borderRadius: '5px',
+                height: '20px',
+                border: 'none',
+                paddingLeft: '35px',
+                color: '#969696', 
+                fontFamily: 'Arial'
+            }}
+        />
+          <SearchIcon style={{ position: 'absolute', left: '55px', top: '20px', fontSize: '20px', color: '#969696' }} />      
+      </div>
       </form>
       {weatherData && (
         <>
