7476f57a65c0e694bd2558559667ef4b13587d8f
Elmeri Sipola <elmerisipola@hotmail.com>
2024-06-06 15:21:57 +0300

    Remove chance of rain

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -3,19 +3,13 @@ const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 export const fetchWeatherData = (city) => {
   return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
     .then(response => response.json())
-    .then(data => ({
-      ...data,
-      chanceOfRain: data.rain ? 100 : 0,
-    }));
+    .then(data => data);
 };
 
 export const fetchExtraWeatherData = (lat, lon) => {
     return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
     .then(response => response.json())
-    .then(data => ({
-      ...data,
-      chanceOfRain: data.rain ? 100 : 0,
-    }));
+    .then(data => data);
 };
 
 export const fetch24hForecast = (lat, lon) => {
