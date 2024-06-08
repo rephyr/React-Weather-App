076466dab9892a9948c196b30bf2aac792a0b678
Elmeri Sipola <elmerisipola@hotmail.com>
2024-06-08 17:15:38 +0300

    Next day time fetch

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -42,8 +42,58 @@ export const fetch24hForecast = (lat, lon) => {
           dt_txt: localTime
         };
       });
-      console.log('24-hour forecast:', forecast24h);
 
       return forecast24h;
     });
+};
+
+export const fetchNextDaySunrise = (lat, lon) => {
+  return new Promise((resolve, reject) => {
+    const today = new Date();
+    const nextDay = new Date(today);
+    nextDay.setDate(today.getDate() + 1);
+
+    const formattedDate = nextDay.toISOString().split('T')[0];
+    const url = `https://api.sunrisesunset.io/json?lat=${lat}&lng=${lon}&date=${formattedDate}`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'OK') {
+          const sunriseLocal = data.results.sunrise;
+
+          const dateTimeParts = sunriseLocal.split(' '); 
+          const timeParts = dateTimeParts[0].split(':'); 
+          let hours = parseInt(timeParts[0], 10);
+          const minutes = timeParts[1];
+          const seconds = timeParts[2];
+          const amPm = dateTimeParts[1];
+      
+          if (amPm === 'PM' && hours < 12) {
+              hours += 12;
+          } else if (amPm === 'AM' && hours === 12) {
+              hours = 0;
+          }
+      
+          const hoursFormatted = hours.toString().padStart(2, '0');
+      
+          // Reconstruct the date-time string in 24-hour format
+          const dateTimeString = `${formattedDate}T${hoursFormatted}:${minutes}:${seconds}`;
+          console.log(dateTimeString);
+          // Create the Date object
+          const sunriseDate = new Date(dateTimeString);
+            
+          // Convert the Date object to a Unix timestamp
+          const sunriseTimestamp = Math.floor(sunriseDate.getTime() / 1000);
+
+          resolve(sunriseTimestamp);
+        } else {
+          reject('Failed to fetch next day\'s sunrise time');
+        }
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+        reject('Error: ' + error);
+      });
+  });
 };
