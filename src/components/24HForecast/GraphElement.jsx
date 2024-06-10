54fcc4c6ea686ac7f2d38c219d0a2c0759417e5d
Elmeri Sipola <elmerisipola@hotmail.com>
2024-06-10 16:32:08 +0300

    Use local time instead of user time

--- src/components/24HForecast/GraphElement.jsx
+++ src/components/24HForecast/GraphElement.jsx
@@ -1,23 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const GraphElement = ({ point, data, index, points, setTooltip })  => {
-    const [hovered, setHovered] = useState(false);
     if (index === points.length - 1) return null;
     const textAnchor = "middle"; 
     const textY = point[1] < 20 ? point[1] + 20 : point[1] - 10;
     const adjustedX = point[0]; 
-    const titleText = `Time: ${new Date(data.dt * 1000).toLocaleTimeString([], 
+    const titleText = `Time: ${new Date(data.dt_txt).toLocaleTimeString([], 
         { hour: '2-digit', minute: '2-digit', hour12: false })}, Temp: ${Math.round(data.main.temp)}°`;
 
     return (
         <g key={index} onMouseEnter={(e) => { setTooltip({ 
-            show: true, x: e.clientX, y: e.clientY, text: titleText }); setHovered(true); }}>
-            <circle cx={adjustedX} cy={point[1]} r={hovered ? 4 : 2} fill="white" />
+            show: true, x: e.clientX, y: e.clientY, text: titleText });}}>
+            <circle cx={adjustedX} cy={point[1]} r={0} fill="white" />
             <text x={adjustedX} y={textY} fontSize="15" fill="white" textAnchor={textAnchor}>
                 {Math.round(data.main.temp)}°
             </text>
             <text x={adjustedX} y={textY + 30} fontSize="13" fill="white" textAnchor="middle">
-                {new Date(data.dt * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
+                {new Date(data.dt_txt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false })}
             </text>
             <text x={adjustedX} y={textY + 50} fontSize="13" fill="white" textAnchor="middle">
                 {Math.round(data.wind.speed)} m/s               
