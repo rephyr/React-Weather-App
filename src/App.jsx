26fa34d81141c7dd29d9506a55026772e96b2096
Elmeri Sipola <elmerisipola@hotmail.com>
2024-06-14 15:16:01 +0300

    Add sevendayforecast

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,36 @@ import './App.css';
 import RainChart from './components/24HRainForecast/RainChart';
 import SideBar from './components/SideBar/SideBar';
 import TopBox from './components/MainLayout/TopBox'; 
+import SevenDayForecastChart from './components/SevenDayForecast/SevenDayForecastChart';
 import TemperatureGraph from './components/24HForecast/TemperatureGraph';
 import useWeatherData from './hooks/useWeatherData';
 
 function App() {
   const [city, setCity] = React.useState('London');
-  const { weatherData, extraWeatherData, forecastData, error } = useWeatherData(city);
+  const { weatherData, extraWeatherData, forecastData, sevenDayForecast, error } = useWeatherData(city);
 
   return (
     <div className="App">
       <div className="background"></div>
-      <SideBar className="sidebar" city={city} setCity={setCity} weatherData={weatherData} extraWeatherData={extraWeatherData} error={error} />
-        <div className="content">
-          <div className="topbox">
-            <TopBox weatherData={weatherData} />
-          </div>
+        <SideBar className="sidebar" city={city} setCity={setCity} weatherData={weatherData} extraWeatherData={extraWeatherData} error={error} />   
+      <div className="content">
+        <div className="topbox">
+          <TopBox weatherData={weatherData} />
+        </div>
+        <div className="chart-container">
           <div className="bottom-container">
             <div className="temperature-graph-container">
-              <TemperatureGraph className="TemperatureGraph" forecastData={forecastData}/>     
+              <TemperatureGraph className="TemperatureGraph" forecastData={forecastData}/>   
             </div>
             <div className="rain-chart-container">
-              <RainChart className="RainChart" forecastData={forecastData}/>     
+              <RainChart className="RainChart" forecastData={forecastData}/>  
             </div>
           </div>
+          <div className="seven-day-chart">
+            <SevenDayForecastChart sevenDayForecast={sevenDayForecast} />
+          </div>
         </div>
+      </div>
     </div>
   );
 }
