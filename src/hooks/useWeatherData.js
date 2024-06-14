4ba4aa245743f08a5178d2aa0e3a2adc89406ccd
Elmeri Sipola <elmerisipola@hotmail.com>
2024-06-14 15:17:35 +0300

    Refactor and new fetch

--- src/hooks/useWeatherData.js
+++ src/hooks/useWeatherData.js
@@ -1,44 +1,52 @@
 import { useState, useEffect } from 'react';
-import { fetchWeatherData, fetchExtraWeatherData, fetch24hForecast } from '../api/weatherApi';
+import { fetchWeatherData, fetchExtraWeatherData, fetch24hForecast, fetch7DayForecast } from '../api/weatherApi';
 
 const useWeatherData = (city) => {
   const [weatherData, setWeatherData] = useState(null);
   const [extraWeatherData, setExtraWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState(null);
+  const [sevenDayForecast, setSevenDayForecast] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetchWeatherData(city)
-      .then(data => {
+    let isMounted = true;
+
+    const fetchData = async () => {
+      try {
+        const data = await fetchWeatherData(city);
+        if (!isMounted) return;
         setWeatherData(data);
 
         const { lat, lon } = data.coord;
-        fetchExtraWeatherData(lat, lon)
-          .then(extraData => {
-            setExtraWeatherData(extraData);
+        const extraData = await fetchExtraWeatherData(lat, lon);
+        if (!isMounted) return;
+        setExtraWeatherData(extraData);
 
-            fetch24hForecast(lat, lon)
-              .then(forecastData => {
-                setForecastData(forecastData);
-              })
-              .catch(err => {
-                setError(`Error fetching 24-hour forecast data: ${err.message}`);
-              });
-          })
-          .catch(err => {
-            setError(`Error fetching extra weather data: ${err.message}`);
-          });
-      })
-      .catch(err => {
+        const forecastData = await fetch24hForecast(lat, lon);
+        if (!isMounted) return;
+        setForecastData(forecastData);
+
+        const fiveDayData = await fetch7DayForecast(lat, lon);
+        if (!isMounted) return;
+        setSevenDayForecast(fiveDayData);
+      } catch (err) {
+        if (!isMounted) return;
         if (err.message.includes('city not found')) {
           setError('City not found. Please try again.');
         } else {
-          setError(`Error fetching weather data: ${err.message}`);
+          setError(`Error fetching data: ${err.message}`);
         }
-      });
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    };
   }, [city]);
 
-  return { weatherData, extraWeatherData, forecastData, error };
+  return { weatherData, extraWeatherData, forecastData, sevenDayForecast, error };
 };
 
 export default useWeatherData;
