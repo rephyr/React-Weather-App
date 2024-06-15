f364674e88a866c79538adb0afa814d30ee4cd7c
Elmeri Sipola <elmerisipola@hotmail.com>
2024-06-15 16:20:07 +0300

    add logic for changing background image

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,28 @@ import TopBox from './components/MainLayout/TopBox';
 import SevenDayForecastChart from './components/SevenDayForecast/SevenDayForecastChart';
 import TemperatureGraph from './components/24HForecast/TemperatureGraph';
 import useWeatherData from './hooks/useWeatherData';
+import getBackgroundImage from './getBackgroundImage';
 
 function App() {
   const [city, setCity] = React.useState('London');
   const { weatherData, extraWeatherData, forecastData, sevenDayForecast, error } = useWeatherData(city);
+  const [backgroundImageUrl, setBackgroundImage] = React.useState('');
 
+  React.useEffect(() => {
+    if (weatherData) {
+      const imageUrl = getBackgroundImage(weatherData.weather[0].main);
+      console.log('Image URL:', imageUrl);
+      setBackgroundImage(imageUrl);
+    }
+  }, [weatherData]);
+  
   return (
     <div className="App">
-      <div className="background"></div>
-        <SideBar className="sidebar" city={city} setCity={setCity} weatherData={weatherData} extraWeatherData={extraWeatherData} error={error} />   
+        <div key={backgroundImageUrl} className="background" style={{ backgroundImage: backgroundImageUrl }}></div>
+      <SideBar className="sidebar" city={city} setCity={setCity} weatherData={weatherData} extraWeatherData={extraWeatherData} error={error} />   
       <div className="content">
         <div className="topbox">
-          <TopBox weatherData={weatherData} />
+          <TopBox weatherData={weatherData} /> 
         </div>
         <div className="chart-container">
           <div className="bottom-container">
